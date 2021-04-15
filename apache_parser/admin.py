c7acd429faa08c8197630cdb4105c5a963605c63
beloslav <velislav1329@gmail.com>
2021-04-15 16:21:43 +0300

    add search fields

--- apache_parser/admin.py
+++ apache_parser/admin.py
@@ -8,6 +8,7 @@ class ApacheLogDataAdmin(admin.ModelAdmin):
     list_display = ('id', 'ip_address', 'created_log', 'http_method', 'url', 'response_code', 'response_size')
     list_display_links = ('id', 'ip_address', 'created_log', 'http_method', 'url', 'response_code', 'response_size')
     list_filter = ('created_log', 'http_method', 'response_code')
+    search_fields = ('id', 'http_method', 'response_code', 'ip_address')
 
 
 admin.site.register(ApacheLogData, ApacheLogDataAdmin)
